ff5a4d8b6288d905930c7b5e7b4b9c625b14bf8b
robingithb <robinthomasq007@gmail.com>
2024-08-26 15:09:09 +0530

    dropdown button and methods of all actions

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,181 @@ class _WaterIntakeAppHomePageState extends State<WaterIntakeAppHomePage> {
     });
   }
 
+  Future<void> _incrementWaterIntake() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      _waterIntake++;
+      pref.setInt('waterIntake', _waterIntake);
+      if (_waterIntake >= _dailyGoal) {
+        //show a dialogue box
+        _goalReachedDialog();
+      }
+    });
+  }
+
+  Future<void> _resetWaterInatke() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      _waterIntake = 0;
+      pref.setInt('waterIntake', _waterIntake);
+    });
+  }
+
+  Future<void> _setDailyGoal(int newGoal) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      _dailyGoal = newGoal;
+      pref.setInt('dailyGoal', newGoal);
+    });
+  }
+
+  Future<void> _goalReachedDialog() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text(
+              "Congratulations...!",
+              style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text(
+                    "You hav reached your daily goal of $_dailyGoal glasses of water...!",
+                    style: const TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "OK",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ))
+            ],
+          );
+        });
+  }
+
+  Future<void> _showResetConfirmationDialog() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text(
+              "Comfirmation",
+              style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            content: const SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text(
+                    "Are you sure you want to reset your water intake?",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "Cancel",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              TextButton(
+                  onPressed: () {
+                    _resetWaterInatke();
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "Reset",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double progress = _waterIntake / _dailyGoal;
+    bool goalReached = _waterIntake >= _dailyGoal;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Water Intake App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Icon(
+                Icons.water_drop,
+                size: 120,
+                color: Colors.blue,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "You have consumed : ",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text('$_waterIntake glasses of water',
+                  style: Theme.of(context).textTheme.headlineSmall),
+              const SizedBox(
+                height: 15,
+              ),
+              LinearProgressIndicator(
+                value: progress,
+                backgroundColor: Colors.grey[300],
+                valueColor: const AlwaysStoppedAnimation(Colors.blue),
+                minHeight: 20,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Daily Goal",
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              DropdownButton(
+                  value: _dailyGoal,
+                  items: _dailyGoalOptions.map((int value) {
+                    return DropdownMenuItem(
+                        value: value, child: Text("$value Glasses"));
+                  }).toList(),
+                  onChanged: (int? newValue) {
+                    if (newValue != null) {
+                      _setDailyGoal(newValue);
+                    }
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
